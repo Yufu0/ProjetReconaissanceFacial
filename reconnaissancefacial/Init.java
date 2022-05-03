3a8d3fa2a5b6bf4dc142e3e09013c56dd29cff4b
Celio BUERI <91431098+Yufu0@users.noreply.github.com>
2022-05-03 21:28:43 +0200

    Update Init.java

--- reconnaissancefacial/Init.java
+++ reconnaissancefacial/Init.java
@@ -8,7 +8,7 @@ public class Init {
     public static final int WIDTH = 300;
     public static final int HEIGHT = 500;
 
-    public static final int EPSILON = 2000;
+    public static final int EPSILON = 3000;
 
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         MySQL database =  MySQL.getInstance();
